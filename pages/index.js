90e2e85c19a8e3bec63aa8ae29b581efecfb761b
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-18 19:33:33 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,11 @@ import Avatar from "../components/Avatar";
 import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import { SearchIcon } from "@heroicons/react/outline";
+import Footer from "../components/Footer";
 
 export default function Home() {
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <Head>
         <title>Google</title>
         <link rel="icon" href="/favicon.ico" />
@@ -33,7 +34,7 @@ export default function Home() {
         </div>
       </header>
 
-      <form className="flex flex-col items-center mt-44 flex-grow">
+      <form className="flex flex-col items-center mt-44 flex-grow w-4/5">
         <Image
           src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
           height={100}
@@ -46,19 +47,20 @@ export default function Home() {
         px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl"
         >
           <SearchIcon className="h-5 mr-3 text-gray-500" />
-          <input
-            type="text"
-            className="flex-grow focus:outline-none "
-          />
+          <input type="text" className="flex-grow focus:outline-none " />
           <MicrophoneIcon className="h-5" />
         </div>
-        <div className="flex flex-col w-1/2 space-y-2 justify-center
-        mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+        <div
+          className="flex flex-col w-1/2 space-y-2 justify-center
+        mt-8 sm:space-y-0 sm:flex-row sm:space-x-4"
+        >
           <button className="btn">Google Search</button>
 
           <button className="btn">I'm Feeling Lucky</button>
         </div>
       </form>
+
+      <Footer/>
     </div>
   );
 }
