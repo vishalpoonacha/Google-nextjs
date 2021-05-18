1a0a4257e5a35c4f8e5e5708d95259718b193fce
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-18 18:53:39 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,8 @@
 import Head from "next/head";
 import Avatar from "../components/Avatar";
 import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
+import Image from "next/image";
+import { SearchIcon } from "@heroicons/react/outline";
 
 export default function Home() {
   return (
@@ -22,11 +24,41 @@ export default function Home() {
           <p className="link">Gmail</p>
           <p className="link">Images</p>
 
-          <ViewGridIcon className="h-10 w-10"/>
+          <ViewGridIcon
+            className="h-10 w-10 p-2
+           rounded-full hover:bg-gray-100 cursor-pointer"
+          />
 
-          <Avatar url="https://coaching.papareact.com/ai9" />
+          <Avatar url="https://yt3.ggpht.com/yti/ANoDKi53W6HlfChCPYFWTiaysGDZ1i23AWUdk5TRWy63lg=s88-c-k-c0x00ffffff-no-rj-mo" />
         </div>
       </header>
+
+      <form className="flex flex-col items-center mt-44 flex-grow">
+        <Image
+          src="https://www.google.co.uk/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png"
+          height={100}
+          width={300}
+        />
+
+        <div
+          className="flex w-full mt-5 hover:shadow-lg 
+        focus-within:shadow-lg max-w-md rounded-full border border-gray-200
+        px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl"
+        >
+          <SearchIcon className="h-5 mr-3 text-gray-500" />
+          <input
+            type="text"
+            className="flex-grow focus:outline-none "
+          />
+          <MicrophoneIcon className="h-5" />
+        </div>
+        <div className="flex flex-col w-1/2 space-y-2 justify-center
+        mt-8 sm:space-y-0 sm:flex-row sm:space-x-4">
+          <button className="btn">Google Search</button>
+
+          <button className="btn">I'm Feeling Lucky</button>
+        </div>
+      </form>
     </div>
   );
 }
