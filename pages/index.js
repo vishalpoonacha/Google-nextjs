70a348ceaaa42e5397d37c10d3232a83f385031c
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-19 13:09:24 +0530

    commit

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,21 @@ import { MicrophoneIcon, ViewGridIcon } from "@heroicons/react/solid";
 import Image from "next/image";
 import { SearchIcon } from "@heroicons/react/outline";
 import Footer from "../components/Footer";
+import { useRef } from "react";
+import { useRouter } from "next/router";
 
 export default function Home() {
+  const router = useRouter();
+  const searchInputRef = useRef(null);
+  const search = (e) => {
+    e.preventDefault();
+
+    const term = searchInputRef.current.value;
+
+    if (!term) return;
+
+    router.push(`/search?term=${term}`);
+  };
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <Head>
@@ -47,20 +60,28 @@ export default function Home() {
         px-5 py-3 items-center sm:max-w-xl lg:max-w-2xl"
         >
           <SearchIcon className="h-5 mr-3 text-gray-500" />
-          <input type="text" className="flex-grow focus:outline-none " />
+          <input
+            ref={searchInputRef}
+            type="text"
+            className="flex-grow focus:outline-none "
+          />
           <MicrophoneIcon className="h-5" />
         </div>
         <div
           className="flex flex-col w-1/2 space-y-2 justify-center
         mt-8 sm:space-y-0 sm:flex-row sm:space-x-4"
         >
-          <button className="btn">Google Search</button>
+          <button onClick={search} className="btn">
+            Google Search
+          </button>
 
-          <button className="btn">I'm Feeling Lucky</button>
+          <button onClick={search} className="btn">
+            I'm Feeling Lucky
+          </button>
         </div>
       </form>
 
-      <Footer/>
+      <Footer />
     </div>
   );
 }
