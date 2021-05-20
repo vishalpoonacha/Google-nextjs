ab47cf2e063c821abcf2d3b9cccbd1f456d4b1f0
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-20 16:03:33 +0530

    commit

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 //API Key - https://developers.google.com/custom-search/v1/using_rest
 
-export const API_KEY = "AIzaSyDAhVwNkhzxUqumnlzydpj8RXy-wVcsutw";
+export const API_KEY = "AIzaSyAmiELbQrY7rHu2JaOVm4iZmlX2P4g7Mtg";
 
 //Context Key - https://cse.google.com/cse/create/new <--**To create context**
 export const CONTEXT_KEY = "72f2dc0cd54d5ad5c";
