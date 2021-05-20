cf54572ccd422ee0b55b1d073705b1d3314d5265
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-20 13:00:43 +0530

    commit

--- pages/search.js
+++ pages/search.js
@@ -1,13 +1,17 @@
 import Head from "next/head";
+import { useRouter } from "next/router";
 import Header from "../components/Header";
+import SearchResults from "../components/SearchResults";
 import { API_KEY, CONTEXT_KEY } from "../keys";
+import Response from "../Response";
 
 function Search({ results }) {
+  const router = useRouter();
   console.log(results);
   return (
     <div>
       <Head>
-        <title>Search Results</title>
+        <title>{router.query.term} - Google Search</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
@@ -15,6 +19,7 @@ function Search({ results }) {
       <Header />
 
       {/*Search Results */}
+      <SearchResults results={results} />
     </div>
   );
 }
@@ -23,10 +28,13 @@ export default Search;
 
 export async function getServerSideProps(context) {
   const useDummyData = false;
+  const startIndex = context.query.start || "0";
 
-  const data = await fetch(
-    `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`
-  ).then((response) => response.json());
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}&start=${startIndex}`
+      ).then((response) => response.json());
 
   //After the SSR pass the results to client
   return {
