293dda045c4e3a4b258128f5d54a8e1cc673169c
vishalpoonacha <vishalpoonacha007@gmail.com>
2021-05-20 16:32:30 +0530

    API

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
-//API Key - https://developers.google.com/custom-search/v1/using_rest
+//API Key - https://developers.google.com/custom-search/v1/using_rest <-- TO GET YOUR API
 
-export const API_KEY = "AIzaSyAmiELbQrY7rHu2JaOVm4iZmlX2P4g7Mtg";
+export const API_KEY = "";
 
 //Context Key - https://cse.google.com/cse/create/new <--**To create context**
 export const CONTEXT_KEY = "72f2dc0cd54d5ad5c";
